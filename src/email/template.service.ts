80f707342ca485e3b014d08057afbcda74207482
Emmanuel Menyaga <sparklinsparky@gmail.com>
2021-01-14 11:33:58 +0100

    templating engine

--- src/email/template.service.ts
+++ src/email/template.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { configure, Environment } from 'nunjucks';
+import { join } from 'path';
+
+import { EMAIL_SERVICE } from '@constants/index';
+import { EmailService } from './interfaces';
 
 @Injectable()
-export class EmailTemplateService {}
+export class EmailTemplateService {
+  private engine: Environment;
+
+  constructor(
+    @Inject(EMAIL_SERVICE) private readonly emailService: EmailService,
+  ) {
+    const path = join(__dirname, './templates');
+    this.engine = configure(path, { autoescape: true });
+  }
+}
